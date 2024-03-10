e9da44c23420498661852ecbe1edabfd55b93b82
htrungtin2901 <httin2901@gmail.com>
2024-03-11 00:05:56 +0700

    tách Header Footer cho vào các Component riêng

--- ShopApp_Angular/src/app/app.module.ts
+++ ShopApp_Angular/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     
   
-    HomeComponent
+    HomeComponent,
+              HeaderComponent,
+              FooterComponent
   ],
   imports: [
     BrowserModule
